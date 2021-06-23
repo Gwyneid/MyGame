06a21620a307bf0b39ddecf6950147226ee5acbd
Gwyneid <71380716+Gwyneid@users.noreply.github.com>
2021-06-24 01:18:51 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+canvas.height = window.innerHeight;
+canvas.width = 1000;
+var screenlength = window.innerWidth;
