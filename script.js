450e6f3a0c7779e233c9e1cc226827c6e06717a8
Gwyneid <71380716+Gwyneid@users.noreply.github.com>
2021-06-24 01:19:12 +1000

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ var ctx = canvas.getContext("2d");
 canvas.height = window.innerHeight;
 canvas.width = 1000;
 var screenlength = window.innerWidth;
+
+document.addEventListener("mousedown", mouseDown, false);
+document.addEventListener("mouseup", mouseUp, false);
+document.addEventListener("keydown",keyDownStart, false);
+document.addEventListener("keydown",move, false);
+document.addEventListener("keyup",stopMove, false);
+document.addEventListener("mousemove", mouseMove, false);
+document.addEventListener("touchstart", touchTrue, false);
+document.addEventListener("touchmove", touchMove, false);
+document.addEventListener("touchcancel", touchFalse, false);
